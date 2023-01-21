9722a8302083e51f0c0461a08071cdde8dea5133
ngekas <nick.gekas@gmail.com>
2023-01-22 10:42:25 +1100

    3 files modified

--- src/app/stepper-overview-example.ts
+++ src/app/stepper-overview-example.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
+import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 
 /**
  * @title Stepper overview
@@ -9,18 +11,45 @@ import {FormBuilder, Validators} from '@angular/forms';
   templateUrl: 'stepper-overview-example.html',
   styleUrls: ['stepper-overview-example.css'],
 })
-export class StepperOverviewExample {
+export class StepperOverviewExample implements OnInit,AfterViewInit {
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
   });
   secondFormGroup = this._formBuilder.group({
     secondCtrl: ['', Validators.required],
   });
-  isLinear = false;
+  @ViewChild('stepper') stepper: MatStepper;
+  @ViewChild('step1') step1: TemplateRef<HTMLElement>;
+  @ViewChild('step2') step2: TemplateRef<HTMLElement>;
+  @ViewChild('done') done: TemplateRef<HTMLElement>;
 
+  formTemplate: TemplateRef<HTMLElement>;
+  
   constructor(private _formBuilder: FormBuilder) {}
-}
 
+  ngOnInit() {
+    //this.formTemplate = this.step1;   
+  }
+
+  ngAfterViewInit() {
+    this.formTemplate = this.step1;
+    this.stepper.selectedIndex = 2;
+  }
+
+  onSelectionChange(event: StepperSelectionEvent) {
+    switch (event.selectedIndex) {
+      case 0:
+        this.formTemplate = this.step1;
+        break;
+      case 1:
+        this.formTemplate = this.step2;
+        break;
+      default:
+        this.formTemplate = this.done;
+        break;
+    }
+  }
+}
 
 /**  Copyright 2023 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
